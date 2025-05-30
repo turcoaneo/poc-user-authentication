c6096700b2c19d7286025755e3931c386126ef22
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2025-05-30 13:15:46 +0300

    Service, controller, swagger. Test examples.

--- src/main/java/org/copilot/user/authentication/service/util/JwtUtil.java
+++ src/main/java/org/copilot/user/authentication/service/util/JwtUtil.java
@@ -1,31 +1,36 @@
 package org.copilot.user.authentication.service.util;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
 
 public class JwtUtil {
     private static final String SECRET_KEY = "someUsefulLargeEnoughSecretKeyToBeAtLeast256Bits";
-    private static final long EXPIRATION_TIME = 86400000; // 1 day
+    private static final long EXPIRATION_TIME = 1; // day
 
     public static String generateToken(String role) {
         return Jwts.builder()
-                .setSubject(role)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .claims().subject(role).and()
+                .issuedAt(Date.from(Instant.now()))
+                .expiration(Date.from(Instant.now().plus(Duration.ofDays(EXPIRATION_TIME))))
+                .signWith(getSigningKey(), Jwts.SIG.HS256)
                 .compact();
     }
 
-    public static String getRoleFromToken(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(SECRET_KEY) // Use correct key type
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+    private static SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
 
-        return claims.getSubject(); // Extract the role from the subject field
+    public static String getRoleFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
     }
 }
